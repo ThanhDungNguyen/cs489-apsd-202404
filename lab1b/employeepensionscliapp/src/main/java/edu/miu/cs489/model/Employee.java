ce5faea480b51f3279befcad0a3f3f914499e7f1
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-04-03 16:59:35 -0500

    Add methods to Employee entity

--- lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/model/Employee.java
+++ lab1b/employeepensionscliapp/src/main/java/edu/miu/cs489/model/Employee.java
@@ -1,4 +1,74 @@
 package edu.miu.cs489.model;
 
+import java.time.LocalDate;
+
 public class Employee {
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private LocalDate employmentDate;
+    private Double yearlySalary;
+
+    public Employee() {
+        this(null, null, null, null, null);
+    }
+
+    public Employee(Long employeeId, String firstName, String lastName, LocalDate employmentDate, Double yearlySalary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employmentDate = employmentDate;
+        this.yearlySalary = yearlySalary;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getEmploymentDate() {
+        return employmentDate;
+    }
+
+    public void setEmploymentDate(LocalDate employmentDate) {
+        this.employmentDate = employmentDate;
+    }
+
+    public Double getYearlySalary() {
+        return yearlySalary;
+    }
+
+    public void setYearlySalary(Double yearlySalary) {
+        this.yearlySalary = yearlySalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", employmentDate=" + employmentDate +
+                ", yearlySalary=" + yearlySalary +
+                '}';
+    }
 }
