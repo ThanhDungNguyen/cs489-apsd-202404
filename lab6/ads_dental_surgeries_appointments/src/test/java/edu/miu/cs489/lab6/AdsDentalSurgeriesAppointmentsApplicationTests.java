fbe00f1933c376e0ad0ab2c4afc91e6f6cbf1e62
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-10 14:53:42 -0500

    Lab 6 - Initialize project

---
+++ lab6/ads_dental_surgeries_appointments/src/test/java/edu/miu/cs489/lab6/AdsDentalSurgeriesAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsDentalSurgeriesAppointmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
