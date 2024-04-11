d5d6d77244ae651a47c5a15999b66b081eb4a8ed
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-10 22:57:33 -0500

    Lab 6 - Add Appointment class

---
+++ lab6/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab6/model/Appointment.java
@@ -0,0 +1,21 @@
+package edu.miu.cs489.lab6.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "appointment")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate dateTime;
+    private String surgeryLocation;
+}
