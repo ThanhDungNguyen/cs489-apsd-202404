45ab963e568b3a2022f7b80a6aedacf5a62a7210
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-17 22:12:41 -0500

    Lab 6 - Add patients and addresses

--- lab6/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab6/AdsDentalSurgeriesAppointmentsApplication.java
+++ lab6/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab6/AdsDentalSurgeriesAppointmentsApplication.java
@@ -30,6 +30,7 @@ public class AdsDentalSurgeriesAppointmentsApplication implements CommandLineRun
 
     @Override
     public void run(String... args) throws Exception {
+        // Add dentists
         var dentist = new Dentist(null, "Tony", "Smith", "111-111-1111", "tony.smith@email.com", "Specialization 1");
         dentistService.addNewDentist(dentist);
 
@@ -39,11 +40,33 @@ public class AdsDentalSurgeriesAppointmentsApplication implements CommandLineRun
         dentist = new Dentist(null, "Robin", "Plevin", "111-111-3333", "robin.plevin@email.com", "Specialization 3");
         dentistService.addNewDentist(dentist);
 
+        // Add patients and addresses
         var address = new Address(null, "Street 1", "City 1", "State 1", "11111");
         addressService.addNewAddress(address);
 
-        var patient = new Patient("P100", "Gillian", "White", "222-222-2222", "gillian.white@email.com",
+        var patient = new Patient("P100", "Gillian", "White", "222-222-1111", "gillian.white@email.com",
                 LocalDate.of(1988, 1, 1), address);
+        patientService.addNewPatient(patient);
+
+        address = new Address(null, "Street 2", "City 2", "State 2", "22222");
+        addressService.addNewAddress(address);
+
+        patient = new Patient("P105", "Jill", "Bell", "222-222-2222", "jill.bell@email.com",
+                LocalDate.of(1988, 2, 2), address);
+        patientService.addNewPatient(patient);
+
+        address = new Address(null, "Street 3", "City 3", "State 3", "33333");
+        addressService.addNewAddress(address);
+
+        patient = new Patient("P108", "Ian", "MacKay", "222-222-3333", "ian.mackay@email.com",
+                LocalDate.of(1988, 3, 3), address);
+        patientService.addNewPatient(patient);
+
+        address = new Address(null, "Street 3", "City 3", "State 3", "33333");
+        addressService.addNewAddress(address);
+
+        patient = new Patient("P110", "John", "Walker", "222-222-4444", "john.walker@email.com",
+                LocalDate.of(1988, 4, 4), address);
         patientService.addNewPatient(patient);
     }
 }
