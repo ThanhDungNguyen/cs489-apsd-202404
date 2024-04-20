699549d64ece4579d6d0d2f015bf5ad703a3041c
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-19 23:17:49 -0500

    Add mapping annotations to PatientController

--- lab7/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab7/controller/PatientController.java
+++ lab7/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab7/controller/PatientController.java
@@ -2,16 +2,24 @@ package edu.miu.cs489.lab7.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
 
 import edu.miu.cs489.lab7.dto.patient.PatientRequest;
 import edu.miu.cs489.lab7.dto.patient.PatientResponse;
 
+@RestController
+@RequestMapping(value = "/adsweb/api/v1/patient")
 public class PatientController {
+    @GetMapping(value = "/list")
     public EntityResponse<List<PatientResponse>> getAllPatients() throws Exception {
         throw new Exception("Not implemented");
     }
 
+    @PostMapping(value = "/add")
     public EntityResponse<PatientResponse> addNewPatient(PatientRequest patientRequest) throws Exception {
         throw new Exception("Not implemented");
     }
