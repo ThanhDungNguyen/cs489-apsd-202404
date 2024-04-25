75efd3194f15d7c5138e6940069af3fb824b3347
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-25 12:37:05 -0500

    Implement security configuration

---
+++ lab9/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab9/ads_dental_surgeries_appointments/config/AdsWebApiSecurityConfig.java
@@ -0,0 +1,62 @@
+package edu.miu.cs489.lab9.ads_dental_surgeries_appointments.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.service.impl.ADSUserDetailsService;
+import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.utils.filter.JWTAuthFilter;
+
+@Configuration
+@EnableWebSecurity
+public class AdsWebApiSecurityConfig {
+    @Autowired
+    private ADSUserDetailsService adsUserDetailsService;
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> {
+                            auth.requestMatchers("/citylibrary/api/v1/service/public/**").permitAll()
+                                    .requestMatchers("/citylibrary/api/v1/publisher/**").authenticated();
+                        })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(adsUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
