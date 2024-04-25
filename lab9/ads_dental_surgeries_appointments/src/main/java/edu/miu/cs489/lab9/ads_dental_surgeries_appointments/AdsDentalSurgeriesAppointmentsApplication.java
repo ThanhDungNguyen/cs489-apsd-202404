4c27803aeae51cd5a8eccf72acdda96805994791
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-25 18:11:18 -0500

    Add initial code to create admin user

--- lab9/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab9/ads_dental_surgeries_appointments/AdsDentalSurgeriesAppointmentsApplication.java
+++ lab9/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab9/ads_dental_surgeries_appointments/AdsDentalSurgeriesAppointmentsApplication.java
@@ -1,13 +1,45 @@
 package edu.miu.cs489.lab9.ads_dental_surgeries_appointments;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.model.Role;
+import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.model.User;
+import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.repository.RoleRepository;
+import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class AdsDentalSurgeriesAppointmentsApplication {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdsDentalSurgeriesAppointmentsApplication.class, args);
+	}
+
+	@PostConstruct
+	public void initAdminUser() {
+		var adminUser = userRepository.findByUsername("admin");
+		if (!adminUser.isPresent()) {
+			List<Role> listAdminRoles = new ArrayList<>();
+			var adminRole = roleRepository.findByName("admin").orElse(new Role(null, "admin", null));
+			listAdminRoles.add(adminRole);
+
+			User newAdminUser = new User(null, "Admin", "Admin", "Admin", "admin",
+					passwordEncoder.encode("test1234"), "admin@fairfieldlibrary.com",
+					listAdminRoles);
+			userRepository.save(newAdminUser);
+		}
 	}
 
 }
--- lab9/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab9/ads_dental_surgeries_appointments/repository/RoleRepository.java
+++ lab9/ads_dental_surgeries_appointments/src/main/java/edu/miu/cs489/lab9/ads_dental_surgeries_appointments/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package edu.miu.cs489.lab9.ads_dental_surgeries_appointments.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.miu.cs489.lab9.ads_dental_surgeries_appointments.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-
+    Optional<Role> findByName(String name);
 }
