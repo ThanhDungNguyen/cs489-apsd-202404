347e1811fb86e9c7248879857e57950e109211cc
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 22:50:27 -0500

    Lab 11 - Task 1b - Add Test Suite

--- lab11/array_reverser/src/test/java/edu/miu/cs489/lab11/task1b/AppTest.java
+++ lab11/array_reverser/src/test/java/edu/miu/cs489/lab11/task1b/AppTest.java
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.BeforeEach;
 
-class AppTest {
+public class AppTest {
     private ArrayFlattenerService arrayFlattenerService;
 
     @BeforeEach
-    void initialize() {
+    public void initialize() {
         arrayFlattenerService = new ArrayFlattenerService();
     }
 
     @Test
-    void flatten2DArrayByServiceShouldWork() {
+    public void flatten2DArrayByServiceShouldWork() {
         int[][] testedArray = { { 1, 3 }, { 0 }, { 4, 5, 9 } };
         int[] expectedArray = { 9, 5, 4, 0, 3, 1 };
         assertArrayEquals(arrayFlattenerService.reverseArray(testedArray), expectedArray);
     }
 
     @Test
-    void flattenNullArrayByServiceShouldWork() {
+    public void flattenNullArrayByServiceShouldWork() {
         assertArrayEquals(arrayFlattenerService.reverseArray(null), null);
     }
 }
---
+++ lab11/array_reverser/src/test/java/edu/miu/cs489/lab11/task1b/ArrayReversorTestCases.java
@@ -0,0 +1,11 @@
+package edu.miu.cs489.lab11.task1b;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { AppTest.class })
+public class ArrayReversorTestCases {
+
+}
