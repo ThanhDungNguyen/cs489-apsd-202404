c810c1324fa434698c76305e17e2fae40e6ffde1
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 21:14:08 -0500

    Lab 11 - Task 1a - Run application

--- lab11/array_flattener/src/main/java/edu/miu/cs489/lab11/task1a/App.java
+++ lab11/array_flattener/src/main/java/edu/miu/cs489/lab11/task1a/App.java
@@ -1,13 +1,16 @@
 package edu.miu.cs489.lab11.task1a;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int[][] myArray = { { 1, 2, 3 }, { 4, 5 }, { 6, 7, 8, 9 } };
+
+        ArrayFlattener arrayFlattener = new ArrayFlattener();
+        Arrays.stream(arrayFlattener.flattenArray(myArray)).forEach(System.out::print);
     }
 }
