6b6c946b6b3f806efa94b153dce47eec0a712c04
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 20:55:34 -0500

    Lab 11 - Task 1a - Add AppTest

---
+++ lab11/ArrayFlattener/src/edu/miu/cs489/lab11/task1a/AppTest.java
@@ -0,0 +1,12 @@
+package edu.miu.cs489.lab11.task1a;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void flattenArrayShouldWork() {
+        assertEquals(2, 1 + 1);
+    }
+}
