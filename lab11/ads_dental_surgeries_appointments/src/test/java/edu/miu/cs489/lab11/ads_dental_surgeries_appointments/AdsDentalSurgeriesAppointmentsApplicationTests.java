8b5a7b82f05de380daf155b936118b15531045be
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-29 09:17:36 -0500

    Lab 11 - Part 2 - Task 1 - Verify getting patient by ID

--- lab11/ads_dental_surgeries_appointments/src/test/java/edu/miu/cs489/lab11/ads_dental_surgeries_appointments/AdsDentalSurgeriesAppointmentsApplicationTests.java
+++ lab11/ads_dental_surgeries_appointments/src/test/java/edu/miu/cs489/lab11/ads_dental_surgeries_appointments/AdsDentalSurgeriesAppointmentsApplicationTests.java
@@ -2,7 +2,7 @@ package edu.miu.cs489.lab11.ads_dental_surgeries_appointments;
 
 import java.time.LocalDate;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +18,7 @@ class AdsDentalSurgeriesAppointmentsApplicationTests {
 	private PatientService patientService;
 
 	@Test
-	void contextLoads() throws DataNotFoundException {
+	void canFindExistingPatient() throws DataNotFoundException {
 		var patientRequest = new PatientRequest("P001", "TestFirstName", "TestLastName", "111-111-1111",
 				"testf.testl@email.com", LocalDate.of(1988, 1, 1), null);
 		patientService.addNewPatient(patientRequest);
@@ -26,7 +26,18 @@ class AdsDentalSurgeriesAppointmentsApplicationTests {
 		var expectedResponse = new PatientResponse("P001", "TestFirstName", "TestLastName", "111-111-1111",
 				"testf.testl@email.com", LocalDate.of(1988, 1, 1), null);
 
-		Assertions.assertThat(patientService.getPatientById("P001")).isEqualTo(expectedResponse);
+		Assertions.assertEquals(patientService.getPatientById("P001"), expectedResponse);
+	}
+
+	@Test
+	void canHandleFindingNonExistingPatient() throws DataNotFoundException {
+		var patientRequest = new PatientRequest("P001", "TestFirstName", "TestLastName", "111-111-1111",
+				"testf.testl@email.com", LocalDate.of(1988, 1, 1), null);
+		patientService.addNewPatient(patientRequest);
+
+		Assertions.assertThrows(DataNotFoundException.class, () -> {
+			patientService.getPatientById("P002");
+		});
 	}
 
 }
