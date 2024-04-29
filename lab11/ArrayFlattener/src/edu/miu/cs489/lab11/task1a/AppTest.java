c29284a1f6afb1545fe423459f8f03ef30fdcbcf
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 21:01:31 -0500

    Lab 11 - Task 1a - Test the ArrayFlattener

--- lab11/ArrayFlattener/src/edu/miu/cs489/lab11/task1a/AppTest.java
+++ lab11/ArrayFlattener/src/edu/miu/cs489/lab11/task1a/AppTest.java
@@ -1,12 +1,22 @@
 package edu.miu.cs489.lab11.task1a;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AppTest {
+    private ArrayFlattener arrayFlattener;
+
+    @Before
+    public void initialize() {
+        arrayFlattener = new ArrayFlattener();
+    }
+
     @Test
     public void flattenArrayShouldWork() {
-        assertEquals(2, 1 + 1);
+        int[][] testedArray = { { 1, 3 }, { 0 }, { 4, 5, 9 } };
+        int[] expectedArray = { 1, 3, 0, 4, 5, 9 };
+        assertArrayEquals(arrayFlattener.flattenArray(testedArray), expectedArray);
     }
 }
