8eeb649fd0bb3236992ca1a3b4da530cdacdbc35
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 22:01:23 -0500

    Lab 11 - Task 1b - Init project

---
+++ lab11/array_reverser/src/test/java/edu/miu/cs489/lab11/task1b/AppTest.java
@@ -0,0 +1,18 @@
+package edu.miu.cs489.lab11.task1b;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
