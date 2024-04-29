19263257c38943978023a02e5dd0fa1aef840a89
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-29 00:03:44 -0500

    Lab 11 - Task 2 - Test finding patient function

--- lab11/ads_dental_surgeries_appointments/src/test/java/edu/miu/cs489/lab11/ads_dental_surgeries_appointments/AdsDentalSurgeriesAppointmentsApplicationTests.java
+++ lab11/ads_dental_surgeries_appointments/src/test/java/edu/miu/cs489/lab11/ads_dental_surgeries_appointments/AdsDentalSurgeriesAppointmentsApplicationTests.java
@@ -1,13 +1,32 @@
 package edu.miu.cs489.lab11.ads_dental_surgeries_appointments;
 
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import edu.miu.cs489.lab11.dto.patient.PatientRequest;
+import edu.miu.cs489.lab11.dto.patient.PatientResponse;
+import edu.miu.cs489.lab11.exception.DataNotFoundException;
+import edu.miu.cs489.lab11.service.PatientService;
 
 @SpringBootTest
 class AdsDentalSurgeriesAppointmentsApplicationTests {
+	@Autowired
+	private PatientService patientService;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws DataNotFoundException {
+		var patientRequest = new PatientRequest("P001", "TestFirstName", "TestLastName", "111-111-1111",
+				"testf.testl@email.com", LocalDate.of(1988, 1, 1), null);
+		patientService.addNewPatient(patientRequest);
+
+		var expectedResponse = new PatientResponse("P001", "TestFirstName", "TestLastName", "111-111-1111",
+				"testf.testl@email.com", LocalDate.of(1988, 1, 1), null);
+
+		Assertions.assertThat(patientService.getPatientById("P001")).isEqualTo(expectedResponse);
 	}
 
 }
