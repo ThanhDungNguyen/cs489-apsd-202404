8e76670b77edd9426a946b64696411dbb9b977e6
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 21:15:36 -0500

    Lab 11 - Task 1a - Add AppTest

--- lab11/array_flattener/src/test/java/edu/miu/cs489/lab11/task1a/AppTest.java
+++ lab11/array_flattener/src/test/java/edu/miu/cs489/lab11/task1a/AppTest.java
@@ -1,20 +1,22 @@
 package edu.miu.cs489.lab11.task1a;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    private ArrayFlattener arrayFlattener;
+
+    @Before
+    public void initialize() {
+        arrayFlattener = new ArrayFlattener();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void flattenArrayShouldWork() {
+        int[][] testedArray = { { 1, 3 }, { 0 }, { 4, 5, 9 } };
+        int[] expectedArray = { 1, 3, 0, 4, 5, 9 };
+        assertArrayEquals(arrayFlattener.flattenArray(testedArray), expectedArray);
     }
 }
