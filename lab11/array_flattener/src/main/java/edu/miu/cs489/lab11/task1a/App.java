4f73453cc9c71af58b5729fe6e91e7bcd41a9375
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 21:09:29 -0500

    Lab 11 - Task 1a - Add new project

---
+++ lab11/array_flattener/src/main/java/edu/miu/cs489/lab11/task1a/App.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.lab11.task1a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lab11/array_flattener/src/test/java/edu/miu/cs489/lab11/task1a/AppTest.java
@@ -0,0 +1,20 @@
+package edu.miu.cs489.lab11.task1a;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
