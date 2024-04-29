7645a4f42b7b1f90d72bcfa8f26eca5ca60e48a6
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-28 21:08:21 -0500

    Lab 11 - Task 1a - Remove wrong project

--- lab11/ArrayFlattener/src/edu/miu/cs489/lab11/task1a/ArrayFlattener.java
+++
@@ -1,9 +0,0 @@
-package edu.miu.cs489.lab11.task1a;
-
-import java.util.Arrays;
-
-public class ArrayFlattener {
-    public int[] flattenArray(int[][] input) {
-        return Arrays.stream(input).flatMapToInt(row -> Arrays.stream(row)).toArray();
-    }
-}
